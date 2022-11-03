5f33f1fa4697f89196697f2fabec1a5fd567a9c6
Gunnarwise <89876042+Gunnarwise@users.noreply.github.com>
2022-11-03 12:26:44 -0700

    convert hex
    I make a hex converter

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Text;
+
+namespace ConvertHex
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            converthex("hello world");
+        }
+
+        static void converthex(string input)
+        {
+            // converts string to list of bytes
+            byte[] text = Encoding.Default.GetBytes(input);
+
+            // converts the list to a string separated by - then lower cases all letters
+            string hex = BitConverter.ToString(text).ToLower();
+            
+
+            // replaces the - with spaces
+            hex = hex.Replace("-", " ");
+            Console.WriteLine(hex);
+        }
+    }
+}
